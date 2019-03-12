95e89ab5fa4d87e343f8de937a06a729a6af30b0
arxitim <arseniy22848@gmail.com>
2019-03-12 14:26:38 +0300

    The ability to write multiple arguments to the repository has been added. Also, now the program checks if the storage file already exists and creates it if it does not exist.

--- storage.py
+++ storage.py
@@ -8,24 +8,30 @@ import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--key", help="Key")
-parser.add_argument("--value", help="value")
+parser.add_argument("--value", nargs="+", help="value")
 args = parser.parse_args()
-storage_path = os.path.join(tempfile.gettempdir(), 'storage.json')
+storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
 
 
 # Запись пары ключ-значения
 if args.key and args.value:
-    with open(storage_path, 'w') as f:
-        data = {args.key:args.value}
-        f.write(json.dumps(data, ensure_ascii=False))
+    if os.path.exists(storage_path) and os.path.getsize(storage_path):
+        with open(storage_path, 'r+', encoding='utf-8') as load_file:
+            data = json.load(load_file)
+        with open(storage_path, 'w', encoding='utf-8') as input_file:
+            data[args.key] = args.value
+            json.dump(data, input_file)
+    else:
+        data = {}
+        data[args.key] = args.value
+        with open(storage_path, 'w', encoding='utf-8') as input_file:
+            json.dump(data, input_file)
 
 # Вывод значения из хранилища
 elif args.key:
-    with open(storage_path, 'r') as f:
-        data = json.load(f)
+    with open(storage_path, 'r') as output_file:
+        data = json.load(output_file)
         try:
-            print(", ".join(data[args.key]))
+            print(', '.join(data[args.key]))
         except KeyError:
             print(None)
-
-
